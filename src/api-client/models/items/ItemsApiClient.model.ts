370b80fed5722b56072391726e1c950c306db1bc
SaeedAdam <sandeed70@gmail.com>
2023-11-19 22:21:31 +0300

    removed api-client dependecy on axios

--- src/api-client/models/items/ItemsApiClient.model.ts
+++ src/api-client/models/items/ItemsApiClient.model.ts
@@ -1,48 +1,42 @@
 // file: src/api-client/models/items/ItemsApiClient.model.ts
-import axios, { AxiosRequestConfig, AxiosError, AxiosResponse } from 'axios'
-import { ItemsApiClientOptions, ItemsApiClientEndpoints } from './ItemsApiClientOpti\
-ons.interface'
-import { ItemsApiClientInterface } from './ItemsApiClient.interface'
-import { ItemInterface } from '@/models'
 
-// file: src/api-client/models/items/ItemsApiClient.model.ts
+import {
+    useHttpClient,
+    HttpRequestParamsInterface,
+    HttpRequestType,
+} from "@/http-client";
+
+import {
+    ItemsApiClientOptions,
+    ItemsApiClientEndpoints,
+} from "./ItemsApiClientOptions.interface";
+import { ItemsApiClientInterface } from "./ItemsApiClient.interface";
+import { ItemInterface } from "@/models";
 
 /**
-* @Name ItemsApiClientModel
-* @description
-* Implements the ItemsApiClientInterface interface
-*/
+ * @Name ItemsApiClientModel
+ * @description
+ * Implements the ItemsApiClientInterface interface
+ */
 export class ItemsApiClientModel implements ItemsApiClientInterface {
-    private readonly endpoints!: ItemsApiClientEndpoints
-    private readonly mockDelay: number = 0
+    private readonly endpoints!: ItemsApiClientEndpoints;
+    private readonly mockDelay: number = 0;
+
     constructor(options: ItemsApiClientOptions) {
-        this.endpoints = options.endpoints
+        this.endpoints = options.endpoints;
         if (options.mockDelay) {
-            this.mockDelay = options.mockDelay
+            this.mockDelay = options.mockDelay;
         }
     }
+
     fetchItems(): Promise<ItemInterface[]> {
-        return new Promise<ItemInterface[]>((resolve) => {
-            const endpoint = this.endpoints.fetchItems
-            // axios options
-            const options: AxiosRequestConfig = {
-                headers: {
-                }
-            }
-            axios
-                .get(endpoint, options)
-                .then((response: AxiosResponse) => {
-                    if (!this.mockDelay) {
-                        resolve(response.data as ItemInterface[])
-                    } else {
-                        setTimeout(() => {
-                            resolve(response.data as ItemInterface[])
-                        }, this.mockDelay)
-                    }
-                })
-                .catch((error: any) => {
-                    console.error('ItemsApiClient: HttpClient: Get: error', error)
-                })
-        })
+        const requestParameters: HttpRequestParamsInterface = {
+            requestType: HttpRequestType.get,
+            endpoint: this.endpoints.fetchItems,
+            requiresToken: false,
+            mockDelay: this.mockDelay,
+        };
+
+        return useHttpClient().request<ItemInterface[]>(requestParameters);
     }
 }
--- src/http-client/models/UrlUtils.ts
+++ src/http-client/models/UrlUtils.ts
@@ -11,7 +11,7 @@ export const UrlUtils: UrlUtilsInterface = {
    * @name getFullUrlWithParams
    * @description Returns the full formatted url for an API end-point
    * by replacing parameters place holder with the actual values.
-   * @param baseUrl The base API end-point witht he params placeholders like {projectId}
+   * @param baseUrl The base API end-point with the params placeholders like {projectId}
    * @param params The request params object with the key/value entries for each parameter
    * @returns The fully formatted API end-point url with the actual parameter values
    */
